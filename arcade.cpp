7d68df326e5a8e9566bab76b089b7ba9e3875e46
petergrillot <petergrillot@gmail.com>
2021-07-23 00:26:42 -0400

    add more games

--- arcade.cpp
+++ arcade.cpp
@@ -1,19 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <jsoncpp/json/json.h>
-#include "data.h"
 #include <fstream>
 #include <chrono>
 #include <thread>
 
-#define SHADER_FILENAME "blur.frag"
 #define TEXT_LEFT 1200
-#define TEXT_TOP 340
-#define BOXART_LEFT 400
+#define TEXT_TOP 400
+#define BOX_LEFT 320
 #define FONT_FILENAME "RobotoCondensed-Regular.ttf"
+#define NO_VALUE "NO_VALUE"
 
 using namespace std;
-using namespace chrono;
 using namespace chrono;
 
 int main()
@@ -42,19 +40,16 @@ int main()
 
   // Animation
   int frame = 0;
-  enum Direction {
+  enum Direction
+  {
     Left,
     Right,
     None
   };
   int animationDirection = None;
 
-  // Shaders (unused for now)
-  if (!sf::Shader::isAvailable())
-  {
-    cerr << "Shader are not available" << endl;
-    return -1;
-  }
+  // Color
+  sf::Color darkGreyColor = sf::Color(23, 23, 23, 255);
 
   //Declare a Font object
   sf::Font font;
@@ -63,66 +58,101 @@ int main()
     cerr << "Error: Cannot load font" << endl;
   }
 
-  // Load shaders
-  sf::Shader shader;
-  if (!shader.loadFromFile(SHADER_FILENAME, sf::Shader::Fragment))
-  {
-    cerr << "Error: Cannot load Shaders" << endl;
-    return -1;
-  }
+  // Black Background
+  sf::RectangleShape blackBackgroundRectangle;
+  blackBackgroundRectangle.setSize(sf::Vector2f(screenWidth * (pageSize + 1), screenHeight));
+  blackBackgroundRectangle.setFillColor(sf::Color(0, 0, 0));
 
   // Var Vectors
-  vector<sf::Texture> bgImage(jsonObjSize);
-  vector<sf::RectangleShape> rect( jsonObjSize);
-  vector<sf::Sprite> bgSprite(jsonObjSize);
-  vector<sf::Sprite> box(jsonObjSize);
-  vector<sf::Texture> boxArt(jsonObjSize);
-  vector<sf::Text> title(jsonObjSize);
-  vector<sf::Text> description(jsonObjSize);
- 
+  vector<sf::RectangleShape> fadeRectangle(jsonObjSize);
+
+  vector<sf::Texture> backgroundTexture(jsonObjSize);
+  vector<sf::Sprite> backgroundSprite(jsonObjSize);
+
+  vector<sf::Sprite> boxSprite(jsonObjSize);
+  vector<sf::Texture> boxTexture(jsonObjSize);
+
+  vector<sf::Sprite> logoSprite(jsonObjSize);
+  vector<sf::Texture> logoTexture(jsonObjSize);
+
+  vector<sf::Text> titleText(jsonObjSize);
+  vector<sf::Text> descriptionText(jsonObjSize);
+
   // Build Each Game Page
   for (size_t i = 0; i < obj.size(); i++)
   {
     int screenWidthOffset = screenWidth * i;
     // Background
+    // 1280w x 720h
     string bgPath = "assets/" + obj[index]["bg"].asString();
-    if (!bgImage[i].loadFromFile(bgPath, sf::IntRect(0, 0, screenWidth, screenHeight)))
+    if (!backgroundTexture[i].loadFromFile(bgPath, sf::IntRect(0, 0, screenWidth, screenHeight)))
       return -1;
-    bgSprite[i].scale(sf::Vector2f(2.1f, 2.1f)); // 
-    bgImage[i].setSmooth(true);
-    bgSprite[i].setTexture(bgImage[i]);
-    bgSprite[i].move(sf::Vector2f(screenWidthOffset, 0));
-    
-    // BG Effect 
-    rect[i].setSize(sf::Vector2f(screenWidth, screenHeight));
-    rect[i].setFillColor(sf::Color(23,23,23,255));
-    rect[i].move(sf::Vector2f(screenWidthOffset, 0));
+    backgroundSprite[i].scale(sf::Vector2f(2.1f, 2.1f)); //
+    backgroundTexture[i].setSmooth(true);
+    backgroundSprite[i].setTexture(backgroundTexture[i]);
+    backgroundSprite[i].move(sf::Vector2f(screenWidthOffset, 0));
+
+    // BG Effect
+    fadeRectangle[i].setSize(sf::Vector2f(screenWidth, screenHeight));
+    fadeRectangle[i].setFillColor(darkGreyColor);
+    fadeRectangle[i].move(sf::Vector2f(screenWidthOffset, 0));
 
     // Box Art
+    // 850w x 1109h
     string barArtPath = "assets/" + obj[index]["box"].asString();
-    if (!boxArt[i].loadFromFile(barArtPath, sf::IntRect(0, 0, 1534, 2100)))
+    if (!boxTexture[i].loadFromFile(barArtPath, sf::IntRect(0, 0, 1534, 2100)))
       return -1;
-    boxArt[i].setSmooth(true);
-    box[i].rotate(-5.f); // offset relative to the current angle
-    box[i].scale(sf::Vector2f(0.8f, 0.8f));
-    box[i].move(sf::Vector2f(BOXART_LEFT + (screenWidthOffset), TEXT_TOP));
-    box[i].setTexture(boxArt[i]);
+    boxTexture[i].setSmooth(true);
+    boxSprite[i].rotate(-5.f);
+    boxSprite[i].scale(sf::Vector2f(0.8f, 0.8f));
+    boxSprite[i].move(sf::Vector2f(BOX_LEFT + screenWidthOffset, 300));
+    boxSprite[i].setTexture(boxTexture[i]);
 
     // Margin
     int marginLeft = screenWidthOffset + TEXT_LEFT;
-    // Title
-    title[i].setFont(font);
-    title[i].setString(obj[index]["title"].asString());
-    title[i].setCharacterSize(120);
-    title[i].setFillColor(sf::Color::White);
-    title[i].move(sf::Vector2f(marginLeft, TEXT_TOP + 40));//Declare a Font object
 
-    // Description
-    description[i].setFont(font);
-    description[i].setString(obj[index]["description"].asString());
-    description[i].setCharacterSize(40);
-    description[i].setFillColor(sf::Color::White);
-    description[i].move(sf::Vector2f(marginLeft, 640));
+    // Logo
+    // 800w x ANYh
+    // If no Logo, use Text
+    if (!obj[index]["logo"].empty())
+    {
+      string logoPath = "assets/" + obj[index]["logo"].asString();
+      if (!logoTexture[i].loadFromFile(logoPath))
+        return -1;
+      titleText[i].setFont(font);
+      titleText[i].setString(NO_VALUE);
+      logoTexture[i].setSmooth(true);
+      logoSprite[i].setTexture(logoTexture[i]);
+      logoSprite[i].move(sf::Vector2f(880 + screenWidthOffset + (logoTexture[i].getSize().x / 2), (TEXT_TOP + 90) - (logoTexture[i].getSize().y / 2)));
+    }
+    else
+    {
+      titleText[i].setFont(font);
+      titleText[i].setString(obj[index]["title"].asString());
+      titleText[i].setCharacterSize(180);
+      titleText[i].setFillColor(sf::Color::White);
+      titleText[i].move(sf::Vector2f(marginLeft, TEXT_TOP + 40));
+    }
+
+    // DescriptionText
+    string longDesc = obj[index]["description"].asString();
+    size_t pos;
+
+    for (size_t i = 0; i < longDesc.size(); i += 65)
+    {
+      int n = longDesc.rfind(' ', i);
+      if (n != std::string::npos)
+      {
+        longDesc.at(n) = '\n';
+      }
+    }
+
+    descriptionText[i].setFont(font);
+    descriptionText[i].setString(longDesc);
+    descriptionText[i].setCharacterSize(40);
+    descriptionText[i].setLineSpacing(1.2);
+    descriptionText[i].setFillColor(sf::Color::White);
+    descriptionText[i].move(sf::Vector2f(marginLeft, TEXT_TOP + 420));
     index++;
   }
 
@@ -135,28 +165,31 @@ int main()
   while (window.isOpen())
   {
     sf::Event event;
-    
+    float alpha = 4.2 * frame;
+
     // Animate Direction
-    cout << frame << endl;
-    rect[pageNumber].setFillColor(sf::Color(23,23,23, -1 * (4*frame)));
+    fadeRectangle[pageNumber].setFillColor(sf::Color(23, 23, 23, -1 * alpha));
     if (animationDirection == Right && frame < frameRate)
     {
       view.move(screenWidth / frameRate, 0);
-      rect[pageNumber-1].setFillColor(sf::Color(23,23,23, 4*frame));
+      fadeRectangle[pageNumber - 1].setFillColor(sf::Color(23, 23, 23, alpha));
       frame++;
-    } else if (animationDirection == Left && frame < frameRate)
+    }
+    else if (animationDirection == Left && frame < frameRate)
     {
       view.move(-screenWidth / frameRate, 0);
-      rect[pageNumber+1].setFillColor(sf::Color(23,23,23, 4*frame));
+      fadeRectangle[pageNumber + 1].setFillColor(sf::Color(23, 23, 23, alpha));
       frame++;
-    } else {
+    }
+    else
+    {
       frame = 0;
       animationDirection = None;
       for (size_t i = 0; i < jsonObjSize; i++)
       {
-        if(pageNumber != i)
+        if (pageNumber != i)
         {
-           rect[i].setFillColor(sf::Color(23,23,23, 255));
+          fadeRectangle[i].setFillColor(darkGreyColor);
         }
       }
     }
@@ -169,19 +202,21 @@ int main()
         window.close();
       // Next: Right
       if (
-        event.type == sf::Event::KeyReleased &&
-        event.key.code == sf::Keyboard::Right && 
-        pageNumber < pageSize
-      ) {
+          event.type == sf::Event::KeyReleased &&
+          event.key.code == sf::Keyboard::Right &&
+          animationDirection == None &&
+          pageNumber < pageSize)
+      {
         animationDirection = Right;
         pageNumber++;
       }
       // Prev: Left
       if (
-        event.type == sf::Event::KeyReleased &&
-        event.key.code == sf::Keyboard::Left && 
-        pageNumber > 0
-      ) {
+          event.type == sf::Event::KeyReleased &&
+          event.key.code == sf::Keyboard::Left &&
+          animationDirection == None &&
+          pageNumber > 0)
+      {
         animationDirection = Left;
         pageNumber--;
       }
@@ -189,14 +224,22 @@ int main()
 
     // Set up and draw
     window.setView(view);
-    
+    window.draw(blackBackgroundRectangle);
+
     for (size_t i = 0; i < obj.size(); i++)
     {
-      window.draw(bgSprite[i]);
-      window.draw(box[i]);
-      window.draw(description[i]);
-      window.draw(title[i]);
-      window.draw(rect[i]);
+      window.draw(backgroundSprite[i]);
+      window.draw(boxSprite[i]);
+      window.draw(descriptionText[i]);
+      if (titleText[i].getString() == NO_VALUE)
+      {
+        window.draw(logoSprite[i]);
+      }
+      else
+      {
+        window.draw(titleText[i]);
+      }
+      window.draw(fadeRectangle[i]);
     }
     window.display();
 
