9478fd5c2dad00bbc2f87e262a3eb1c7623d1f88
petergrillot <petergrillot@gmail.com>
2021-07-25 22:05:05 -0400

    Add build

--- compiler.sh
+++ compiler.sh
@@ -4,10 +4,13 @@ g++ bin/arcade.o -o bin/arcade-app -ljsoncpp -lsfml-graphics -lsfml-window -lsfm
 if [ "$1" == "run" ]; then
   bin/arcade-app
 elif [ "$1" == "build" ]; then
-  mkdir -p build
-  cp assets/* build/
+  mkdir -p build/assets/
+  cp assets/* build/assets/
   cp data.json build/
   cp bin/arcade-app build/
+  cp RobotoCondensed-Regular.ttf build/
+  zip -q veranda.zip build
+  rm -r build
 else
   echo "compiled in /bin"
 fi
